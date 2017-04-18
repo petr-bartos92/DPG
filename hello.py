49ef42e9d99824430d59b8c45ad2a388d5eaeff3
petr-bartos92 <petr.bartos92@gmail.com>
2017-04-18 11:38:47 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,3 +4,12 @@ import multiprocessing as mp
 print("Python version %s " % str(sys.version))
 print("cores %s " % str(mp.cpu_count()))
 
+if True:
+  print("tady")
+else:
+  print("Tady ne")
+  a = 0
+  b = 1
+  c = b /a
+  
+  
